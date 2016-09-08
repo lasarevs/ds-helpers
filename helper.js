3e1b3969f784a61e41efe7448a07aa71ec46fd28
crasher <lasarevs@rambler.ru>
2016-09-08 23:40:37 +0300

    edit 3

--- helper.js
+++ helper.js
@@ -1,17 +1,17 @@
 // JavaScript
 console.log('helper is loaded');
 
-$( document ).ready(function() {
-    $('body').on('resize', function() {
+$(document).ready(function () {
+    $('body').on('resize', function () {
         //location.reload();
     })
 });
 
 function fitChart(params) {
-    var chart = $('.qlik_chart');
-    var width = (params.width || $('body').width()) - 215;
+    var chart  = $('.qlik_chart');
+    var width  = (params.width || $('body').width()) - 215;
     var height = (params.height || $('body').height()) - 115;
-    if(height <  $('.filter_block').height()) {
+    if (height < $('.filter_block').height()) {
         height = $('.filter_block').height();
     }
     chart.width(width);
