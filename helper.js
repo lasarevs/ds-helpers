5cbf618181c2f1ac87bd93c910d66573a574a56c
crasher <lasarevs@rambler.ru>
2016-09-08 22:53:26 +0300

    first commit

---
+++ helper.js
@@ -0,0 +1,17 @@
+// JavaScript
+$( document ).ready(function() {
+    $('body').on('resize', function() {
+        //location.reload();
+    })
+});
+
+function fitChart(params) {
+    var chart = $('.qlik_chart');
+    var width = (params.width || $('body').width()) - 215;
+    var height = (params.height || $('body').height()) - 115;
+    if(height <  $('.filter_block').height()) {
+        height = $('.filter_block').height();
+    }
+    chart.width(width);
+    chart.height(height);
+}
